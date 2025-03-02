79b740d1c6a0367b796e45ddb9fcd1c79620e1df
Youssouf SISSOKO <ysissoko78@gmail.com>
2025-03-02 12:05:01 +0100

    added file push streal

---
+++ src/services/file-push-stream.ts
@@ -0,0 +1,114 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+// pull in the required packages.
+import * as sdk from "microsoft-cognitiveservices-speech-sdk";
+import * as fs from "fs";
+
+function ReadInt32(fd: number) {
+    let buffer = Buffer.alloc(4);
+    let bytesRead = fs.readSync(fd, buffer);
+    if (4 != bytesRead) {
+        throw "Error reading 32-bit integer from .wav file header. Expected 4 bytes. Actual bytes read: " + String(bytesRead);
+    }
+    return buffer.readInt32LE();
+}
+
+function ReadUInt16(fd: number) {
+    let buffer = Buffer.alloc(2);
+    let bytesRead = fs.readSync(fd, buffer);
+    if (2 != bytesRead) {
+        throw "Error reading 16-bit unsigned integer from .wav file header. Expected 2 bytes. Actual bytes read: " + String(bytesRead);
+    }
+    return buffer.readUInt16LE();
+}
+
+function ReadUInt32(fd: number) {
+    let buffer = Buffer.alloc(4);
+    let bytesRead = fs.readSync(fd, buffer);
+    if (4 != bytesRead) {
+        throw "Error reading unsigned 32-bit integer from .wav file header. Expected 4 bytes. Actual bytes read: " + String(bytesRead);
+    }
+    return buffer.readUInt32LE();
+}
+
+function ReadString(fd: number, length: number) {
+    let buffer = Buffer.alloc(length);
+    let bytesRead = fs.readSync(fd, buffer);
+    if (length != bytesRead) {
+        throw "Error reading string from .wav file header. Expected " + String(length) + " bytes. Actual bytes read: " + String(bytesRead);
+    }
+    return buffer.toString();
+}
+
+export const openPushStream = (filename: string) => {
+
+    // Get the wave header for the file.
+    let wavFileHeader = readWavFileHeader(filename);
+
+    let format: sdk.AudioFormatTag;
+
+    switch (wavFileHeader.tag) {
+        case 1: // PCM
+            format = sdk.AudioFormatTag.PCM;
+            break;
+        case 6:
+            format = sdk.AudioFormatTag.ALaw;
+            break;
+        case 7:
+            format = sdk.AudioFormatTag.MuLaw;
+            break;
+        default:
+            throw new Error("Wave format " + wavFileHeader.tag + " is not supported");
+    }
+
+    // Create the format for PCM Audio.
+    let fmt: sdk.AudioStreamFormat = sdk.AudioStreamFormat.getWaveFormat(wavFileHeader.framerate, wavFileHeader.bitsPerSample, wavFileHeader.nChannels, format);
+
+    // create the push stream we need for the speech sdk.
+    let pushStream = sdk.AudioInputStream.createPushStream(fmt);
+
+    // open the file and push it to the push stream. 
+    // Notice: we skip 44 bytes for the typical wav header.
+    fs.createReadStream(filename, { start: 44 }).on('data', function (arrayBuffer) {
+        pushStream.write(arrayBuffer.slice() as ArrayBuffer);
+    }).on('end', function () {
+        pushStream.close();
+    });
+
+    return pushStream;
+};
+
+export const readWavFileHeader = (audioFileName: string) => {
+    let fd = fs.openSync(audioFileName, 'r');
+
+    if (ReadString(fd, 4) != "RIFF") {
+        throw "Error reading .wav file header. Expected 'RIFF' tag.";
+    }
+    // File length
+    ReadInt32(fd);
+    if (ReadString(fd, 4) != "WAVE") {
+        throw "Error reading .wav file header. Expected 'WAVE' tag.";
+    }
+    if (ReadString(fd, 4) != "fmt ") {
+        throw "Error reading .wav file header. Expected 'fmt ' tag.";
+    }
+    // Format size
+    let formatSize = ReadInt32(fd);
+    if (formatSize > 16) {
+        throw "Error reading .wav file header. Expected format size 16 bytes. Actual size: " + String(formatSize);
+    }
+    // Format tag
+    let tag = ReadUInt16(fd);
+    let nChannels = ReadUInt16(fd);
+    let framerate = ReadUInt32(fd);
+    // Average bytes per second
+    ReadUInt32(fd);
+    // Block align
+    ReadUInt16(fd);
+    let bitsPerSample = ReadUInt16(fd);
+
+    fs.closeSync(fd);
+
+    return { framerate: framerate, bitsPerSample: bitsPerSample, nChannels: nChannels, tag: tag };
+};
